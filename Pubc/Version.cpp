d93df1849b917fb7cb6734db73d02dd6213c6274
Jeroen D Stout <jeroenstout@gmail.com>
2019-05-19 13:41:08 +0200

    Add template files

---
+++ Pubc/Version.cpp
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "[TEMPLATE]/Pubc/Version.h"
+
+#include "[TEMPLATE]/-genc/def_repo_version.h"
+#include "[TEMPLATE]/-genc/def_contribute.h"
+
+namespace [TEMPLATE] {
+	namespace Core {
+		
+		BR_VERSION_DEFINE([TEMPLATE]);
+        BR_CONTRIBUTE_DEFINE([TEMPLATE]);
+
+	}
+} 
